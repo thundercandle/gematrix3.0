42010deba58117299f1de6e9599f5b67712ce498
Michael Sharpe <m.sharpe@project89.org>
2018-01-05 13:35:41 -0800

    Added and merged CRN and CRNA output. Basic working example.

---
+++ client/App.js
@@ -0,0 +1,10 @@
+import App from './src/App';
+import { AppRegistry } from 'react-native';
+
+// This is the placeholder for the main app in the stv code.  Allows react native
+// to access it.
+//
+// Could refactor to allow for an access point for custom mobile code with
+// with react-native.  Currently, I do not believe CRA allows for unique file
+// extensions to define device-specific code.
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import App from './App';
+import { AppRegistry } from 'react-native';
+import registerServiceWorker from './registerServiceWorker';
+
+AppRegistry.registerComponent('App', () => App);
+AppRegistry.runApplication('App', { rootTag: document.getElementById('root') });
+
+registerServiceWorker();
