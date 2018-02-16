8875ca31f70b317460e81bf5cb730702e3aa1856
Michael Sharpe <m.sharpe@project89.org>
2018-02-16 13:28:39 -0800

    Working on responsive routing.

--- client/src/modules/notebooks/index.js
+++ client/src/modules/notebooks/index.js
@@ -1,15 +1,99 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-native'
+import { Switch, Route, Redirect } from 'react-router-native'
 import { View, Text } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
 
-import { CoreLayout, AppRoute } from './../core'
+import { CoreLayout, AppRoute, MediaQuery, breakpoints, Link } from './../core'
 
-export class Notebooks extends Component {
+const DetailLayout = ({ children }) => (
+  <View style={{flex: 2, backgroundColor: '#4e4e4e', height: '100%'}}>
+    { children }
+  </View>
+)
+
+const Dashboard = () => (
+  <DetailLayout><Text>Dashboard!</Text></DetailLayout>
+)
+
+
+const Notebook = ({ match }) => (
+  <DetailLayout><Text>Notebook: {match.params.id}</Text></DetailLayout>
+)
+
+const notes = [
+  {
+    title: 'test',
+    id: 1234567890
+  },
+  {
+    title: 'test2',
+    id: 1234567891
+  },
+  {
+    title: 'test3',
+    id: 1234567892
+  },
+  {
+    title: 'test4',
+    id: 1234567893
+  },
+  {
+    title: 'test5',
+    id: 1234567894
+  },
+  {
+    title: 'test6',
+    id: 1234567895
+  }
+]
+
+const NotebookNav = ({}) => (
+  <View style={{height: '100%', flex: 1, backgroundColor: '#4e4e4e'}}>
+    <List containerStyle={{marginBottom: 20}}>
+      {
+        notes.map((n, i) => (
+          <Link to={`/notebooks/${n.id}`} key={i}>
+            <View>
+              <ListItem
+                title={n.title}
+              />
+            </View>
+          </Link>
+        ))
+      }
+    </List>
+  </View>
+)
+
+const MobileRoutes = () => (
+  <Switch>
+    <Route exact path="/notebooks" component={NotebookNav}/>
+    <Route exact path="/notebooks/dashboard" component={Dashboard}/>
+    <Route path="/notebooks/:id" component={Notebook}/>
+  </Switch>
+)
+
+const DesktopRoutes = () => (
+  <View style={{flexDirection: 'row', height: '100%'}}>
+    <NotebookNav/>
+    <Switch>
+      <Route path="/notebooks/dashboard" component={Dashboard}/>
+      <Route path="/notebooks/:id" component={Notebook}/>
+      <Redirect from="/notebooks" to="/notebooks/dashboard"/>
+    </Switch>
+  </View>
+)
+
+class Notebooks extends Component {
   render() {
     return (
-      <View>
-        <Text>Notebooks</Text>
-      </View>
+      <MediaQuery isSmaller={breakpoints.tablet}>
+        { isSmaller =>
+            isSmaller
+            ? <MobileRoutes/>
+            : <DesktopRoutes/>
+        }
+      </MediaQuery>
     )
   }
 }
@@ -17,7 +101,7 @@ export class Notebooks extends Component {
 export const NotebookRoutes = () => {
   return (
     <Switch>
-      <AppRoute exact path="/notebooks" component={Notebooks} layout={CoreLayout}/>
+      <AppRoute path="/notebooks" component={Notebooks} layout={CoreLayout}/>
     </Switch>
   )
 }
