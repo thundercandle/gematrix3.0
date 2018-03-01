5603b1b4fb635aaed5221b6add11d0a2438778e5
Michael Sharpe <m.sharpe@project89.org>
2018-02-28 16:05:00 -0800

    Working pattern for multiple sidebar views fo desktop vs mobile.

--- client/src/modules/notebooks/index.js
+++ client/src/modules/notebooks/index.js
@@ -5,6 +5,111 @@ import { List, ListItem } from 'react-native-elements'
 
 import { CoreLayout, AppRoute, MediaQuery, breakpoints, Link } from './../core'
 
+const notes = [
+  {
+    title: 'test',
+    id: 1234567890,
+    numerals: [
+      { value: 1 },
+      { value: 2 },
+      { value: 3 },
+      { value: 4 }
+    ]
+  },
+  {
+    title: 'test2',
+    id: 1234567891,
+    numerals: [
+      { value: 12 },
+      { value: 22 },
+      { value: 33 },
+      { value: 44 },
+      { value: 55 },
+    ]
+  },
+  {
+    title: 'test3',
+    id: 1234567892,
+    numerals: [
+      { value: 4 },
+      { value: 9 },
+      { value: 123 },
+      { value: 111 },
+      { value: 53 },
+    ]
+  },
+  {
+    title: 'test4',
+    id: 1234567893,
+    numerals: [
+      { value: 4},
+      { value: 5 },
+      { value: 89 },
+      { value: 47 },
+      { value: 30 },
+    ]
+  },
+  {
+    title: 'test5',
+    id: 1234567894,
+    numerals: [
+      { value: 49},
+      { value: 53 },
+      { value: 93 },
+      { value: 475 },
+      { value: 300 },
+    ]
+  },
+  {
+    title: 'test6',
+    id: 1234567895,
+    numerals: [
+      { value: 45},
+      { value: 765 },
+      { value: 890 },
+      { value: 471 },
+      { value: 306 },
+    ]
+  }
+]
+
+const ListNotebooks = () => (
+  <List containerStyle={{marginBottom: 20}}>
+    { notes.map((n, i) => (
+        <Link to={`/notebooks/${n.id}`} key={i}>
+          <View>
+            <ListItem
+              title={n.title}
+            />
+          </View>
+        </Link>
+    ))}
+  </List>
+)
+
+const NotebookContent = ({match}) => {
+  const notebook = notes.filter(note => note.id === +match.params.id)
+  console.log(notebook)
+
+  return (
+    <List containerStyle={{marginBottom: 20}}>
+      { notebook[0].numerals.map((n, i) => (
+        <ListItem key={i} title={n.value} hideChevron={true}/>
+      ))}
+    </List>
+  )
+}
+
+const NotebookNav = () => (
+  <View style={{height: '100%', flex: 1, backgroundColor: '#4e4e4e'}}>
+    <Switch>
+      <Route exact path="/notebooks" component={ListNotebooks}/>
+      <Route exact path="/notebooks/dashboard" component={ListNotebooks}/>
+      <Route exact path="/notebooks/:id" component={NotebookContent}/>
+    </Switch>
+  </View>
+)
+
 const DetailLayout = ({ children }) => (
   <View style={{flex: 2, backgroundColor: '#4e4e4e', height: '100%'}}>
     { children }
@@ -15,54 +120,8 @@ const Dashboard = () => (
   <DetailLayout><Text>Dashboard!</Text></DetailLayout>
 )
 
-
 const Notebook = ({ match }) => (
   <DetailLayout><Text>Notebook: {match.params.id}</Text></DetailLayout>
-)
-
-const notes = [
-  {
-    title: 'test',
-    id: 1234567890
-  },
-  {
-    title: 'test2',
-    id: 1234567891
-  },
-  {
-    title: 'test3',
-    id: 1234567892
-  },
-  {
-    title: 'test4',
-    id: 1234567893
-  },
-  {
-    title: 'test5',
-    id: 1234567894
-  },
-  {
-    title: 'test6',
-    id: 1234567895
-  }
-]
-
-const NotebookNav = ({}) => (
-  <View style={{height: '100%', flex: 1, backgroundColor: '#4e4e4e'}}>
-    <List containerStyle={{marginBottom: 20}}>
-      {
-        notes.map((n, i) => (
-          <Link to={`/notebooks/${n.id}`} key={i}>
-            <View>
-              <ListItem
-                title={n.title}
-              />
-            </View>
-          </Link>
-        ))
-      }
-    </List>
-  </View>
 )
 
 const MobileRoutes = () => (
